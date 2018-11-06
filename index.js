b988cba5fc8f38fab159245af4959bb6b655f8ce
Rhian Evans <rhian.evans100@btinternet.com>
2018-11-06 19:55:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your code in this file!
+function scuberGreetingForFeet(distance) {
+  if (distance <= 400) {
+    return "This one is on me!";
+  }
+
+   if (distance >= 2000) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  
+  else if (distance > 2500); {
+    return 'No can do.';
+  }
+}
+  
+  function ternaryCheckCity(NYC) {
+   if (NYC) 
+   return "Ok, sounds good"
+    else return 'No go.';
+   }
+
+   
